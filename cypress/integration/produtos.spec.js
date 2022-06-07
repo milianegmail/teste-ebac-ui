15b3ef1dfd2e9ff579c4f6e625c12dd7e1cc64a3
milianegmail <miliane.madeira@gmail.com>
2022-06-07 10:11:10 -0300

    alteracoes no teste automatizado

---
+++ cypress/integration/produtos.spec.js
@@ -0,0 +1,33 @@
+///reference types="cypress" />
+var faker = require('faker');
+
+describe('Funcionalidade Página de Produtos', () => {
+
+    beforeEach(() => {
+        cy. visit('http://lojaebac.ebaconline.art.br/produtos/')
+    });
+
+    it('Deve selecionar um produto da lista', () =>{
+        cy.get('[class="product-block grid"]')
+            //.first()
+            //.last()
+            //.eq(3)
+            .contains('Abominable Hoodie')
+            .click()
+    });
+
+    it('Deve adicionar um produto no carrinho', () =>{
+        var qtd = 3
+
+        cy.get('[class="product-block grid"]')
+            .contains('Abominable Hoodie').click()
+        cy.get('.button-variable-item-L').click()
+        cy.get('.button-variable-item-Green').click()
+        cy.get('.input-text').clear().type(qtd)
+        cy.get('.single_add_to_cart_button').click()
+
+        cy.get('.dropdown-toggle > .mini-cart-items').should('contain', qtd)
+        cy.get('.woocommerce-message').should('contain', qtd + ' × “Abominable Hoodie” foram adicionados no seu carrinho.')
+
+    });
+})
